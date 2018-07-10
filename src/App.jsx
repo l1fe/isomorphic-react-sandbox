1d09de0d9b2bfee0a62074c7fac5677c6eed55b5
Igor Naumov <igor.naumov.17@gmail.com>
2018-07-10 21:34:02 +0300

    Added React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const App = () => (
+  <div>
+    <h1>Isomorphic React App</h1>
+  </div>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-console.info('React is cool');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const render = (_App) => {
+  ReactDOM.render(<_App/>, document.getElementById('AppContainer'));
+}
+
+render(App);
